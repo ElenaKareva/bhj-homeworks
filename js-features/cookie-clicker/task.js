e0d0a8f1c11a0937c010dde375cc28c5c5155852
ElenaKareva <92863528+ElenaKareva@users.noreply.github.com>
2023-03-11 20:04:25 +0300

    added js file

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,17 @@
+const image = document.getElementById('cookie');
+const counter = document.getElementById('clicker__counter');
+console.log(clicker__counter)
+
+function clickAction() {
+  counter.textContent ++;
+    if(counter.textContent % 2 != 0) {
+      image.width = 250;
+    } else {
+      image.width = 200;
+    }
+
+    if(counter.textContent == 41) {
+      counter.textContent = 1;
+    }
+}
+image.onclick = clickAction;
