c6ae4cdb1e9dbfe6f0e8c418903f13def0018334
ElenaKareva <92863528+ElenaKareva@users.noreply.github.com>
2023-03-19 19:42:52 +0300

    added js file

---
+++ document-structure/todo/task.js
@@ -0,0 +1,26 @@
+const taskInput = document.getElementById('task__input');
+const tasksList = document.getElementById('tasks__list');
+const tasksAdd = document.querySelector('.tasks__add');
+
+tasksAdd.addEventListener('click', (event) => {
+  event.preventDefault();
+  if(taskInput.value.trim()) {
+    tasksList.insertAdjacentHTML('beforeend', `
+      <div class="task">
+        <div class="task__title">
+          ${taskInput.value}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+      </div>
+      `);
+    taskInput.value = '';
+      
+    const task = Array.from(tasksList.querySelectorAll('.task'));
+    const taskRemoveAll = Array.from(tasksList.querySelectorAll('.task__remove'));
+    
+    const index = taskRemoveAll.length - 1;
+    taskRemoveAll[index].onclick = function() {
+      task[index].remove();
+    }
+  }
+})
