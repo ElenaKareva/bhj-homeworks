6147c6792388e1921e73e73cfd3ea3a70b128887
ElenaKareva <92863528+ElenaKareva@users.noreply.github.com>
2023-03-19 12:48:11 +0300

    added js file

---
+++ html-forms/interests/task.js
@@ -0,0 +1,18 @@
+const interestsMain = document.querySelector('.interests_main');
+interestsMain.addEventListener('change', () => {
+  const headTarget = event.target.closest('.interest');
+  const children = headTarget.querySelectorAll('.interests');
+  const parent = headTarget.querySelector('.interest__check');
+
+  children.forEach(item => {
+  const checkBox = item.querySelectorAll('.interest__check');
+  console.log(checkBox);
+    for(let i = 0; i < checkBox.length; i++) {
+      if (parent.checked) {
+        checkBox[i].checked = true;
+      } else {
+        checkBox[i].checked = false;
+      }
+    }
+  });
+});
