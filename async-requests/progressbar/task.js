05b091958ec2fda6e9ae40eef0379937af4d935d
ElenaKareva <92863528+ElenaKareva@users.noreply.github.com>
2023-03-20 15:28:40 +0300

    added js file

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,24 @@
+const progress = document.getElementById('progress');
+progress.value = 0.7;
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const form = document.getElementById('form');
+  const formData = new FormData(form);
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+
+  xhr.upload.onprogress = function(event) {
+    progress.value = event.loaded / event.total;
+  }
+
+  xhr.addEventListener('readystatechange', (event) => {
+    if (xhr.status === 200) {
+      xhr.abort();
+    }
+  })
+  
+  
+  xhr.send(formData);
+});
